ef69ef00b99bd5fdc73acc089e0b5ab79058d792
Peony_Martin <peonymartin007@gmail.com>
2026-03-18 12:52:41 -0400

    Apply updated dashboard from stash@{1}

--- client/src/pages/UserDashboard.jsx
+++ client/src/pages/UserDashboard.jsx
@@ -1,8 +1,21 @@
 import React from "react";
 import useAuth from "../hooks/useAuth";
+<<<<<<< Updated upstream
 import { useNavigate } from "react-router-dom";
 import Sidebar from "../components/sidebarcomponent";
 import "../css-files-pages/userDashboard.css";
+=======
+import { useNavigate, Link } from "react-router-dom";
+import "../css-files-pages/userDashboard.css";
+import {    FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { 
+  faChartColumn, 
+  faFileCirclePlus, 
+  faFileLines, 
+  faGears, 
+  faDoorOpen 
+} from '@fortawesome/free-solid-svg-icons';
+>>>>>>> Stashed changes
 
 const UserDashboard = () => {
   const { logout } = useAuth();
@@ -15,7 +28,38 @@ const UserDashboard = () => {
 
   return (
     <div className="dashboard">
+<<<<<<< Updated upstream
       <Sidebar handleLogout={handleLogout} role="user" activePage="dashboard" />
+=======
+      <aside className="sidebar">
+        <div className="sidebar-heading1">
+          <img src={img} alt="UniMaintain" className="uni-maintain-logo" />
+        </div>
+        <nav className="sidebar-nav">
+            <div className="sidebar-menu active">
+                <FontAwesomeIcon icon={faChartColumn} className="nav-icon" />
+                <span>Dashboard</span>
+            </div>
+            <Link to="/reportform" className="sidebar-menu">
+                <FontAwesomeIcon icon={faFileCirclePlus} className="nav-icon" />
+                <span>Create Report</span>
+            </Link>
+            <div className="sidebar-menu">
+                <FontAwesomeIcon icon={faFileLines} className="nav-icon" />
+                <span>Report Log</span>
+            </div>
+            <div className="sidebar-menu">
+                <FontAwesomeIcon icon={faGears} className="nav-icon" />
+                <span>Settings</span>
+            </div>
+
+            <div className="sidebar-menu" onClick={handleLogout}>
+                <FontAwesomeIcon icon={faDoorOpen} className="nav-icon" />
+                <span>Logout</span>
+            </div>
+            </nav>
+      </aside>
+>>>>>>> Stashed changes
 
       <main className="user-dashboard">
         <div className="dashboard-heading">
