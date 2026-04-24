a55d4293a65e177a3f60a84a4789b60ffd2629ed
BabyBlob <robloxdog@outlook.com>
2026-04-23 20:08:16 -0400

    Spelling Fix

--- client/src/pages/UserManagement.jsx
+++ client/src/pages/UserManagement.jsx
@@ -25,7 +25,7 @@ const UserManagement = () => {
   const maintenanceRoles = [
     "Electrican",
     "Plumber",
-    "General Tecnhician",
+    "General Technician",
     "IT Technician",
     "Groundsman",
   ];
